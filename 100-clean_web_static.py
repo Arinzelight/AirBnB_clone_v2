6bb53994664697935a046598995b72b992d5ee78
Arinzelight <arinzelight2@gmail.com>
2024-01-14 21:12:41 -0800

    update files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # Fabfile to delete out-of-date archives.
-import os
-from fabric.api import *
+
+from fabric.api import env, local, run, lcd, cd
 
 env.hosts = ["34.207.64.162", "52.201.221.50"]
-
+env.user = "ubuntu"
+env.key_filename = ["~/.ssh/id_rsa"]
 
 def do_clean(number=0):
     """Delete out-of-date archives.
@@ -18,13 +19,17 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    # Local cleanup
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        local("ls -tr | head -n -{} | xargs -I {{}} rm -f {{}}".format(number))
 
+    # Remote cleanup
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
+        archives = run("sudo ls -tr | grep web_static_").split()
         [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        [run("sudo rm -rf {}".format(a)) for a in archives]
+
+# Run the fabric command
+if __name__ == "__main__":
+    local("fab -f 100-clean_web_static.py do_clean:number=2")
+
