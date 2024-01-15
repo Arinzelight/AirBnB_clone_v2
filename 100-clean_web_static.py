37b772930ebb401ce36fe4535ff031c6198a3519
Arinzelight <arinzelight2@gmail.com>
2024-01-15 12:24:51 -0800

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,35 +1,92 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
-from fabric.api import env, local, run, lcd, cd
 
 env.hosts = ["34.207.64.162", "52.201.221.50"]
-env.user = "ubuntu"
-env.key_filename = ["~/.ssh/id_rsa"]
+"""The list of host server IP addresses."""
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
+
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
+    """Deletes out-of-date archives of the static files.
     Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+        number (Any): The number of archives to keep.
     """
-    number = 1 if int(number) == 0 else int(number)
-
-    # Local cleanup
-    with lcd("versions"):
-        local("ls -tr | head -n -{} | xargs -I {{}} rm -f {{}}".format(number))
-
-    # Remote cleanup
-    with cd("/data/web_static/releases"):
-        archives = run("sudo ls -tr | grep web_static_").split()
-        [archives.pop() for i in range(number)]
-        [run("sudo rm -rf {}".format(a)) for a in archives]
-
-# Run the fabric command
-if __name__ == "__main__":
-    local("fab -f 100-clean_web_static.py do_clean:number=2")
-
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
